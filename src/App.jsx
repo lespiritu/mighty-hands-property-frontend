6bf9a633f585f4c85f220ff5995cb88c9dfb882b
espirituleoncio7 <espirituleoncio7@gmail.com>
2023-06-01 12:28:08 +0800

    fixed nav

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,16 @@ function App() {
   return (
     <>
       <Navigation/>
-      <Hero/>
+      <Hero />
+      
+      <div className='h-screen bg-slate-800'>
+        <h1>sample</h1>
+        <p>Exercitation tempor laborum ex nostrud culpa Lorem esse quis commodo tempor ea anim nulla non. Est do ad veniam non enim enim eu do consectetur. Consequat sint pariatur eu nisi reprehenderit ex ut ea elit dolore sunt mollit fugiat cillum. Officia voluptate tempor ex esse. Commodo dolore cillum cillum velit qui nisi id id officia sint irure non pariatur qui.</p>
+      </div>
+      <div className='h-screen bg-red-800'>
+        <h1>sample</h1>
+        <p>Exercitation tempor laborum ex nostrud culpa Lorem esse quis commodo tempor ea anim nulla non. Est do ad veniam non enim enim eu do consectetur. Consequat sint pariatur eu nisi reprehenderit ex ut ea elit dolore sunt mollit fugiat cillum. Officia voluptate tempor ex esse. Commodo dolore cillum cillum velit qui nisi id id officia sint irure non pariatur qui.</p>
+      </div>
     </>
   )
 }
--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,28 +1,35 @@
+import { useState } from "react"
 
 
 export function Navigation() {
+    const [closeBtn, setCloseBtn]= useState(true)
     return (
-        <div className='flex justify-between items-center h-screen md:h-auto fixed md:w-screen z-10 bg-cream-1 p-4 md:p-10 top-0 text-zinc-500 '>
-            <div className="hidden md:flex items-center gap-4">
-                <img className='h-20' src="https://res.cloudinary.com/dbed2fwkj/image/upload/v1685549843/logo_btebvc.png" />
-                <div className="flex flex-col">
-                    <p className='text-2xl font-semibold  self-start px-4 pt-2 bg-zinc-700 text-cream-1'>Mighty Hands</p>
-                    <b className='text-2xl font-semibold  self-start px-4 pb-2 bg-zinc-700 text-cream-1'>Property</b>
-
-                    
-                    {/* <p className="font-normal text-xs">contact: +639157230777</p> */}
+        <div className=' md:flex justify-between items-center  fixed w-full z-10 bg-cream-1 p-4 md:p-10 top-0 text-zinc-500 '>
+            <div className="flex items-center gap-2 md:gap-4 ">
+                <img className='h-[50px] md:h-20' src="https://res.cloudinary.com/dbed2fwkj/image/upload/v1685549843/logo_btebvc.png" />
+                <div className="md:flex flex-col">
+                    <p className='text-sm md:text-2xl font-semibold  self-start p-1 md:p-2 bg-zinc-700 text-cream-1'>Mighty Hands</p>
+                    <b className='text-sm md:text-2xl font-semibold  self-start p-1 md:p-2 bg-zinc-700 text-cream-1'>Property</b>
                 </div>
             </div>
 
-            <div className="flex items-center justify-center md:w-auto w-screen">
+           
+            <div className={`flex flex-row items-center justify-center md:relative fixed bg-cream-1 md:z-auto z-[20] right-0 w-full md:w-auto h-screen md:h-auto transition-all duration-300 ease-linear ${closeBtn? "translate-x-[100%]" : "translate-x-[0]"} md:top-auto md:translate-x-0 top-0`}>
               
-                <ul className="flex flex-col md:flex-row ">
+                <ul className="md:flex flex-col md:flex-row md:py-0 py-4 ">
                     
-                    <li className="cursor-pointer text-2xl md:text-base  p-4">Home</li>
-                    <li className="cursor-pointer text-2xl md:text-base  p-4">Property</li>
-                    <li className="cursor-pointer text-2xl md:text-base  p-4">About Us</li>
-                    <li className="cursor-pointer text-2xl md:text-base  p-4">Contact</li>
+                    <li onClick={()=> setCloseBtn(true)} className="cursor-pointer text-xl md:text-base  p-2">Property</li>
+                    <li onClick={()=> setCloseBtn(true)} className="cursor-pointer text-xl md:text-base  p-2">About Us</li>
+                    <li onClick={()=> setCloseBtn(true)} className="cursor-pointer text-xl md:text-base  p-2">Home</li>
+                    <li onClick={()=> setCloseBtn(true)} className="cursor-pointer text-xl md:text-base  p-2">Contact</li>
                 </ul>
+            </div>
+
+
+             <div onClick={()=> setCloseBtn(previous=> !previous)} className="absolute top-6 right-6 z-20 md:hidden flex flex-col gap-[5px] cursor-pointer ">
+                <button className={`origin-top-left bg-zinc-700 h-[3px] rounded-full ${closeBtn? "rotate-[45deg]": "rotate-0"}  w-[24px]  transition-all duration-300 ease-linear`}></button>
+                <button className={` bg-zinc-700 h-[3px] rounded-full  ${closeBtn? "w-0": "w-[24px]"} transition-all duration-300  ease-linear`}></button>
+                <button className={`origin-bottom-left tranlate-x-[100%] bg-zinc-700 h-[3px] rounded-full w-[24px] ${closeBtn? "rotate-[-45deg]": "rotate-0"} transition-all duration-300 ease-linear`}></button>
             </div>
         </div>
     )
